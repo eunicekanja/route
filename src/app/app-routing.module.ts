979cce53a85afdf09acd0cffe69038dd90a9d031
Eunice Kanja <kanjawambui30@gmail.com>
2022-03-29 21:08:18 +0300

    init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CssComponent } from './css/css.component';
+import { HtmlComponent } from './html/html.component';
+import { JavaComponent } from './java/java.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'java',
+    component:JavaComponent
+  },
+  {
+    path:'html',
+    component:HtmlComponent
+  },
+  {
+    path:'css',
+    component:CssComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,36 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { JavaComponent } from './java/java.component';
+import { HtmlComponent } from './html/html.component';
+import { CssComponent } from './css/css.component';
+//import { Routes,RouterModule } from '@angular/router';
 
+// const appRoutes:Routes=[
+//   {
+//     path:'java',
+//     component:JavaComponent
+//   },
+//   {
+//     path:'html',
+//     component:HtmlComponent
+//   },
+//   {
+//     path:'css',
+//     component:CssComponent
+//   },
+// ]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    JavaComponent,
+    HtmlComponent,
+    CssComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    //RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
